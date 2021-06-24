75b3f8cb53ac85d79e9f031dd929510d4d014055
Michael Citro <mikecit22@gmail.com>
2021-06-24 18:38:05 -0400

    Revert "added redis"
    This reverts commit a796ecd95db92a9f80fdcf427cf614f12897cebf.

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const express = require('express')
-const redis = require('redis')
 const app = express()
 const port = 8080
-
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-app.get('/redis', (req, res) => {
-  redis.createClient({
-    host: req.query.host,
-    port: req.query.port,
-  }).on('ready', function (error) {
-    if (error) {
-      res.send('FAILED!')
-    }
-    res.send('CONNECTED!')
-  })
-})
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
